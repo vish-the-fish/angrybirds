e768e8e4e132cb9de5a4e6239ecd61016eceb749
vish-the-fish <69439271+vish-the-fish@users.noreply.github.com>
2020-09-22 19:40:50 -0400

    Add files via upload

--- Pig.js
+++ Pig.js
@@ -2,6 +2,19 @@ class Pig extends BaseClass {
   constructor(x, y){
     super(x,y,50,50);
     this.image = loadImage("sprites/enemy.png");
+    this.visibilty = 255;
   }
-
+display(){
+  if(this.body.speed<3){
+    super.display();
+    console.log(this.body.speed);
+  }else{
+    World.remove(world,this.body);
+    push ();
+    this.visibilty -=5;
+    tint(255, this.visibilty);
+    image (this.image,this.body.position.x,this.body.position.y,50,50);
+    pop ();
+  }
+}
 };
